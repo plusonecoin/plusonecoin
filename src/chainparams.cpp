288e5d4e601969cdc10327ab7f2bcf24541db922
Freddie Ackerman <ackerman2718@gmail.com>
2017-09-12 14:16:57 +0100

    Changed address prefix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -132,7 +132,7 @@ public:
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.push_back(CDNSSeedData("54.172.129.75", "54.172.129.75"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,55);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
