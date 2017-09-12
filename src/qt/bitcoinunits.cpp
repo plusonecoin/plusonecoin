744a6a79c82663d67128f3d5cc53123d4f41c421
Freddie Ackerman <ackerman2718@gmail.com>
2017-09-12 13:50:09 +0100

    Branding of qt client

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case BTC: return QString("PLUS1");
+    case mBTC: return QString("mPLUS1");
+    case uBTC: return QString::fromUtf8("μPLUS1");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Bitcoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("PlusOneCoins");
+    case mBTC: return QString("Milli-PlusOneCoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-PlusOneCoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
